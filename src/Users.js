d1a25d21c4c9ae31cb7ccee9e07431cc2df2362f
ashish.behera <abehera2@sapient.com>
2021-05-31 07:24:01 +0530

    Initial Commit

---
+++ src/Users.js
@@ -0,0 +1,26 @@
+const { getMaxListeners } = require("process");
+
+const users = [
+    {
+        id: 1,
+        name: 'Luke',
+        email: 'luke@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Martin',
+        email: 'martin@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Boriss',
+        email: 'boriss@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Mark',
+        email: 'mark@gmail.com'
+    }
+]
+
+module.exports = users;
